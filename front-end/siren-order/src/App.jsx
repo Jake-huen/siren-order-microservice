5b176066e981674c3d85c087b78353485dcde77a
Jake-huen <tae77777@naver.com>
2024-03-11 15:01:26 +0900

    feat : Front-end 메인화면

---
+++ front-end/siren-order/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Navigation from "./components/Navigation";
+import Main from "./components/Main";
+
+const App = () => {
+
+    return (
+        <div>
+            <Navigation />
+            <Main />
+        </div>
+    );  
+};
+
+export default App;
---
+++ front-end/siren-order/src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = () => {
+    return (
+        <nav className="container">
+            <div className="logo">
+                <img className="top-logo" src="/images/brand_logo.png" alt="" />
+            </div>
+
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+
+            <button>login</button>
+        </nav>
+    );
+
+}
+
+export default Navigation;
