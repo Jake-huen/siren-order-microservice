550738e503c6b5c6c9ffc286389fb32dd6265cf4
Jake-huen <tae77777@naver.com>
2024-03-18 15:30:06 +0900

    #8 Counter-Service 배포

--- counter-service/src/main/java/org/orchestro/counterservice/config/KafkaProducerConfig.java
+++ counter-service/src/main/java/org/orchestro/counterservice/config/KafkaProducerConfig.java
@@ -22,7 +22,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, RequestedReceiptDto> factory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.10.40.152:9092");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(props);
--- counter-service/src/main/java/org/orchestro/counterservice/controller/CounterController.java
+++ counter-service/src/main/java/org/orchestro/counterservice/controller/CounterController.java
@@ -86,9 +86,4 @@ public class CounterController {
                 + ", token secret=" + env.getProperty("token.secret")
                 + ", token expiration time =" + env.getProperty("token.expiration_time"));
     }
-
-//    @GetMapping("/config-check")
-//    public String configCheck() {
-//        return env.getProperty("")
-//    }
 }
